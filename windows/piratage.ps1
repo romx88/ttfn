56795b966237ac76e0988d107228f571f645422e
RVPablo <pablo.rey@ynov.com>
2024-02-27 15:47:26 +0100

    windows add piratage+solution and file test

---
+++ windows/piratage.ps1
@@ -0,0 +1,14 @@
+# Liste des sites à débloquer
+$sites = @("chat.openai.com", "teams.microsoft.com", "youtube.com", "github.com", "ytrack.learn.ynov.com", "extranet.ynov.com", "outlook.office.com")
+
+# Chemin vers le fichier HOSTS
+$hostsFile = "$env:SystemRoot\System32\drivers\etc\hosts"
+
+# Parcourir la liste des sites à débloquer
+foreach ($site in $sites) {
+    # Supprimer l'entrée du fichier HOSTS, si elle existe
+    (Get-Content $hostsFile) | Where-Object { $_ -notmatch "^127\.0\.0\.1\s+$site" } | Set-Content $hostsFile
+    Write-Host "Le site $site a été débloqué."
+}
+
+Write-Host "La suppression des entrées a été effectuée. Les sites spécifiés sont maintenant accessibles."
