589b617955bb9b6668375df4fd760dff296696b4
Bernard <romeo.bernard@ynov.com>
2024-02-27 14:54:35 +0100

    update version blocage on linux

--- suppbackground.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Chemin vers le fichier de configuration du fond d'écran
-config_file="/changebackground.sh"
-
-# Autoriser les modifications du fond d'écran en tant qu'administrateur
-echo "Pour autoriser les modifications ultérieures, entrez votre mot de passe administrateur :"
-sudo chattr -i "$config_file"
