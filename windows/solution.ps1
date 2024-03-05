773efd81f3123991b57613c1fdf4ae71ed915619
unknown <romeo.bernard@ynov.com>
2024-03-05 12:23:02 +0100

    update the solution.ps1

--- windows/solution.ps1
+++ windows/solution.ps1
@@ -7,21 +7,22 @@ $hostsFile = "$env:SystemRoot\System32\drivers\etc\hosts"
 # Adresse IP locale (peut être modifiée selon vos besoins)
 $redirectIP = "127.0.0.1"
 
-# Lire tout le contenu du fichier HOSTS
-$currentContent = Get-Content $hostsFile -Raw
+# Vérifier si le script est exécuté en tant qu'administrateur
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Le script doit être exécuté en tant qu'administrateur pour modifier le fichier hosts."
+    exit
+}
 
 # Parcourir la liste des sites à bloquer
 foreach ($site in $sites) {
     # Vérifier si l'entrée existe déjà dans le fichier HOSTS
-    $existingEntry = "$redirectIP $site"
+    $existingEntry = "$redirectIP`t$site"
+    $pattern = "\b$([regex]::Escape($site))\b"
     
-    if ($currentContent -notlike "*$existingEntry*") {
+    if (-not (Get-Content $hostsFile | Select-String -Pattern $pattern)) {
         # Ajouter une entrée dans le fichier HOSTS pour rediriger le site vers l'adresse IP locale
-        $currentContent += "`n$redirectIP $site"
+        Add-Content -Path $hostsFile -Value "$redirectIP`t$site"
     }
 }
-
-# Réécrire le fichier HOSTS avec le nouveau contenu
-Set-Content -Path $hostsFile -Value $currentContent
 
 Write-Host "Tout trafic sortant vers les sites bloqués est maintenant redirigé vers l'adresse IP locale."
